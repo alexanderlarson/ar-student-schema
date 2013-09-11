04c4f98c3497fc637b22a709309bb0ee84f5b75c
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 16:38:08 -0500

    Added some validations. Please.

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,9 +3,19 @@ require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, :uniqueness => true
+  validates :email, :format => { :with => /\w+@\w+\.[a-zA-Z]{2,}/,
+    :message => "Must be a valid email address" }
+  validates :age, :numericality => { :greater_than_or_equal_to => 5, 
+    :message => "Student must be at least %{count}." }
+  validates :phone, :format => { :with => /\D?\d{3}\D{0,2}\d{3}\D?\d{4}/,
+    :message => "Must be a valid phone number. Please." }
+
+
   def name
     first_name + " " + last_name
   end
+
   def age
     Time.now.utc.to_date.year - birthday.year
   end
