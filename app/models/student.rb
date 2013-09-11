e90ff9d2eb603b4fbb13e7b7f8668b05e53c87be
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 16:03:26 -0500

    Added name and age methods to Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  def name
+    first_name + " " + last_name
+  end
+  def age
+    Time.now.utc.to_date.year - birthday.year
+  end
+
 end
